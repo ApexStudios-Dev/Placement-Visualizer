83c7e64c0693db46ee75e7aeb93bf609b4b4d9e0
Apex <robsonlawson3@gmail.com>
2025-10-23 04:30:49 +0100

    Setup datagen

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import dev.apexstudios.gradle.single.ApexSingleExtension
+
 plugins {
     id("apex-conventions.neoforge") version "0.1.74"
     id("apex-conventions.maven-publishing") version "0.1.74"
@@ -7,3 +9,6 @@ group = "dev.apexstudios"
 
 apex.neoVersion("21.10.20-beta", "1.21.9", "2025.10.05")
 apex.extendCompilerErrors()
+
+val single = ApexSingleExtension.getOrCreate(project)
+single.withDataGen()
