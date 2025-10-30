1df508acab7a076c805afea8885e415d4e91ba42
Apex <robsonlawson3@gmail.com>
2025-10-30 19:35:41 +0000

    Add support for rendering block entities

---
+++ src/main/java/dev/apexstudios/placementvisualizer/impl/GhostVertexConsumer.java
@@ -0,0 +1,77 @@
+package dev.apexstudios.placementvisualizer.impl;
+
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import org.jetbrains.annotations.Nullable;
+
+public final class GhostVertexConsumer implements VertexConsumer {
+    public static final GhostVertexConsumer INSTANCE = new GhostVertexConsumer();
+    public static final int GHOST_ALPHA = 190;
+
+    @Nullable
+    private VertexConsumer delegate;
+
+    private GhostVertexConsumer() {
+    }
+
+    public void setDelegate(@Nullable VertexConsumer delegate) {
+        this.delegate = delegate;
+    }
+
+    public void reset() {
+        delegate = null;
+    }
+
+    @Override
+    public VertexConsumer addVertex(float x, float y, float z) {
+        if(delegate != null) {
+            delegate.addVertex(x, y, z);
+        }
+
+        return this;
+    }
+
+    @Override
+    public VertexConsumer setColor(int r, int g, int b, int a) {
+        if(delegate != null) {
+            delegate.setColor(r, g, b, (a * GHOST_ALPHA) / 0xFF);
+        }
+
+        return this;
+    }
+
+    @Override
+    public VertexConsumer setUv(float u, float v) {
+        if(delegate != null) {
+            delegate.setUv(u, v);
+        }
+
+        return this;
+    }
+
+    @Override
+    public VertexConsumer setUv1(int u, int v) {
+        if(delegate != null) {
+            delegate.setUv1(u, v);
+        }
+
+        return this;
+    }
+
+    @Override
+    public VertexConsumer setUv2(int u, int v) {
+        if(delegate != null) {
+            delegate.setUv2(u, v);
+        }
+
+        return this;
+    }
+
+    @Override
+    public VertexConsumer setNormal(float normalX, float normalY, float normalZ) {
+        if(delegate != null) {
+            delegate.setNormal(normalX, normalY, normalZ);
+        }
+
+        return this;
+    }
+}
