fda82e2f391db3d168a8d64743e6efe08f8705a8
Apex <robsonlawson3@gmail.com>
2025-11-01 15:19:00 +0000

    Revert "Dummy commit"
    This reverts commit 407497d3c15a3e53b51b2401c1be3bde3572d69d.

--- src/main/java/dev/apexstudios/placementvisualizer/impl/PlacementVisualizer.java
+++ src/main/java/dev/apexstudios/placementvisualizer/impl/PlacementVisualizer.java
@@ -23,8 +23,6 @@ public final class PlacementVisualizer {
                 false,
                 Pack.Position.TOP
         ));
-
-        // TODO: Remove me
     }
 
     public static ResourceLocation identifier(String identifier) {
