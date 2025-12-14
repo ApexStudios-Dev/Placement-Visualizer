8be8d67c164fd6be8c3c6fae431bb217a122689e
Apex <robsonlawson3@gmail.com>
2025-12-14 07:05:34 +0000

    Bump ApexGradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import dev.apexstudios.gradle.single.ApexSingleExtension
 
 plugins {
-    id("apex-conventions.neoforge") version "0.1.75"
-    id("apex-conventions.maven-publishing") version "0.1.75"
+    id("apex-conventions.neoforge") version "0.1.85"
+    id("apex-conventions.maven-publishing") version "0.1.85"
 }
 
 group = "dev.apexstudios"
