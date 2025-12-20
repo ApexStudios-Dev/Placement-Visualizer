220e840210dcd91d64819488557c951b6d89b909
Apex <robsonlawson3@gmail.com>
2025-12-20 17:53:48 +0000

    Setup datagen

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,9 +5,54 @@ plugins {
 
 group = "dev.apexstudios"
 
-neoForge.enable {
-    version = "26.1.0.0-alpha.1+snapshot-1"
-    isDisableRecompilation = providers.environmentVariable("CI").map(String::toBoolean).getOrElse(false)
+sourceSets {
+    main {
+        resources.srcDir(file("src/data/generated"))
+    }
+
+    create("data") {
+        java.setSrcDirs(files("src/data"))
+        resources.setSrcDirs(files())
+
+        compileClasspath += main.get().output
+        runtimeClasspath += main.get().output
+
+        tasks.findByName("compileJava")?.finalizedBy(compileJavaTaskName)
+        tasks.findByName("classes")?.finalizedBy(classesTaskName)
+        tasks.findByName("processResources")?.finalizedBy(processResourcesTaskName)
+    }
+}
+
+neoForge {
+    enable {
+        version = "26.1.0.0-alpha.1+snapshot-1"
+        isDisableRecompilation = providers.environmentVariable("CI").map(String::toBoolean).getOrElse(false)
+        enabledSourceSets = setOf(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME], sourceSets["data"])
+    }
+
+    val dataMod = mods.create("data") {
+        sourceSet(sourceSets[SourceSet.MAIN_SOURCE_SET_NAME])
+        sourceSet(sourceSets["data"])
+    }
+
+    runs.create("data") {
+        if(versionCapabilities.splitDataRuns()) {
+            clientData()
+        } else {
+            data()
+        }
+
+        sourceSet.set(sourceSets["data"])
+        loadedMods.set(listOf(dataMod))
+        ideName.set("Data")
+
+        programArguments.addAll(
+            "--mod", "placementvisualizer",
+            "--all",
+            "--output", file("src/data/generated").absolutePath,
+            "--existing", file("src/${SourceSet.MAIN_SOURCE_SET_NAME}/resources").absolutePath
+        )
+    }
 }
 
 repositories {
